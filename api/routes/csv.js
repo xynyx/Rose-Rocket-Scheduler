3024b87db3508a1eceb7be20cd995f8de5658ea9
xynyx <matt.taylr22@gmail.com>
2020-07-29 17:45:13 -0400

    Data properly being added to csv; almost done

--- api/routes/csv.js
+++ api/routes/csv.js
@@ -25,41 +25,80 @@ router.post("/", (req, res) => {
 
   console.log("data :>> ", data);
 
-  data.sort((a, b) => moment(a.startDate) - moment(b.startDate));
+  // data.sort((a, b) => moment(a.startDate) - moment(b.startDate));
 
   const formattedData = () => {
     const { interval, year } = req.body.downloadScheduleOptions;
 
-    let startingDate = moment([year]);
-    const endingDate = moment([year + 1]);
+    console.log("year :>> ", year);
+    const startingDate = moment([year]);
+    const endingDate = moment(startingDate).endOf("year");
+
+    // console.log("startingDate :>> ", startingDate);
+    // console.log("endingDate :>> ", endingDate);
 
     const formattedData = [];
 
     let day = 1;
     /*     while (moment(startingDate).isBefore(endingDate)) { */
+    const startingDateCopy = startingDate.clone();
+
     const generateIntervalObject = () => {
       const intervalObject = {
         timeframe: `Day ${day} - Day ${day + interval - 1}`,
       };
+
       let pickup = 0,
         dropoff = 0,
         other = 0;
-      for (let i = 0; i < interval; i++) {
-        data.forEach(appointment => {
-          const { title } = appointment;
-          const startDate = moment(appointment.startDate);
-          if (
-            startDate.isBetween(
-              startingDate.startOf("day"),
-              startingDate.endOf("day")
-            )
-          ) {
-            if (title === "Pickup") pickup++;
-            if (title === "Dropoff") dropoff++;
-            if (title === "Other") other++;
-          }
-        });
-      }
+      // for (let i = 0; i < interval; i++) {
+      // const copiedStartDate = startingDateCopy.clone();
+      // console.log("***********");
+      data.forEach(appointment => {
+        const { title } = appointment;
+        const startDate = moment(appointment.startDate);
+
+        // console.log(
+        //   'here ',
+        //   startDate.isBetween(
+        //     startingDateCopy.startOf("day"),
+        //     startingDateCopy.add(interval - 1, "days").endOf("day")
+        //   )
+        // );
+        /* 
+        // console.log("startDate :>> ", startDate);
+        // console.log("START:>> ", startingDateCopy.startOf("day"));
+        // console.log(
+        //   "END:>> ",
+        //   startingDateCopy.add(interval - 1, "d").endOf("day")
+        // ); */
+
+        // console.log(
+        //   'TRUE?',
+        //   moment("2020-07-29T16:03:21-04:00").isBetween(
+        //     "2020-07-26T00:00:00-04:00",
+        //     "2020-07-29T23:59:59-04:00"
+        //   )
+        // );
+        const start = startingDateCopy.clone().startOf("day");
+        const end = startingDateCopy.clone().add(interval - 1, "d").endOf("day");
+
+        // console.log(
+        //   "startDate.isBetween(start, end) :>> ",
+        //   moment(startDate).isBetween(start, end)
+        // );
+
+        // moment("12-25-1995", "MM-DD-YYYY");
+        console.log(moment("12-25-1995", "MM-DD-YYYY"));
+
+        if (startDate.isBetween(start, end)) {
+          console.log("HERE!");
+          if (title === "Pickup") pickup++;
+          if (title === "Dropoff") dropoff++;
+          if (title === "Other") other++;
+        }
+      });
+      // }
 
       intervalObject.pickup = pickup;
       intervalObject.dropoff = dropoff;
@@ -68,7 +107,26 @@ router.post("/", (req, res) => {
       formattedData.push(intervalObject);
     };
 
-    generateIntervalObject();
+    // generateIntervalObject();
+    // bugged - no idea why
+    // console.log('endingDate :>> ', endingDate);
+
+    // console.log(
+    //   "startingDate.isBefore(endingDate) :>> ",
+    //   startingDate.isBefore(endingDate)
+    // );
+    // console.log("startingDate :>> ", startingDate);
+    // console.log("endingDate :>> ", endingDate);
+
+    while (startingDate.isBefore(endingDate)) {
+      // console.log('startingDateINSIDE BFERORE :>> ', startingDate);
+      generateIntervalObject();
+      day += interval;
+      startingDate.add(interval, "d");
+      // console.log('startingDateAFTER :>> ', startingDate);
+
+      // console.log('endingDate :>> ', endingDate);
+    }
 
     return formattedData;
   };
