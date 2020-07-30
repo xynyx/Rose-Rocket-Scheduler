80f3c13073f78489731f0741037a16bb87195fe1
xynyx <matt.taylr22@gmail.com>
2020-07-30 11:17:21 -0400

    Deployment

--- api/app.js
+++ api/app.js
@@ -9,6 +9,10 @@ const csvRoutes = require("./routes/csv");
 
 app.use("/api/csv", csvRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}!`));
